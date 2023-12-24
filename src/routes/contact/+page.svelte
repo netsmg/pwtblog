4b365b4e39c4bccdd867a4d7c81575975a7b70f2
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-24 19:04:11 +0600

    Update +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -15,12 +15,15 @@
     // Push the form data to the "contacts" collection
     await push(contactsRef, formData);
 
-    // Optional: Clear the form after submission
+    // Clear the form after submission
     formData = {
       name: "",
       email: "",
       message: "",
     };
+
+    // Alert after submission
+    alert("Form submit successful!");
 
     console.log("Form submitted to Firebase Realtime Database!");
   };
@@ -29,16 +32,22 @@
 <style>
   /* Add your Tailwind styles here */
 </style>
-
-<form on:submit={handleSubmit}>
+<div class="text-2xl font-hind font-bold">Contact Form</div>
+<div class="variant-glass p-5 rounded-md flex flex-col gap-2">
+    <form on:submit={handleSubmit}>
   <label for="name">Name:</label>
-  <input type="text" id="name" bind:value={formData.name} />
+  <input type="text" class="input" id="name" bind:value={formData.name} />
 
   <label for="email">Email:</label>
-  <input type="email" id="email" bind:value={formData.email} />
+  <input type="email" id="email" class="input" bind:value={formData.email} />
 
   <label for="message">Message:</label>
-  <textarea id="message" rows="4" bind:value={formData.message}></textarea>
+  <textarea class="input" id="message" rows="4" bind:value={formData.message}></textarea>
 
-  <button type="submit">Submit</button>
+  <button type="submit" class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
 </form>
+
+  
+</div>
+
+
