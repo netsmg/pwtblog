818e003bac248c624f3b1795b4361ea9cfdc614e
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-24 20:35:43 +0600

    Create +page.svelte

---
+++ src/routes/exam/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { onMount } from 'svelte';
+  import { getDatabase, ref, onValue } from 'firebase/database';
+
+  let exams = [];
+
+  onMount(() => {
+    // Initialize Firebase database
+    const db = getDatabase();
+
+    // Reference to 'jachai/exams/practice'
+    const examsRef = ref(db, 'jachai/exams/practice');
+
+    // Fetch exams from Firebase
+    onValue(examsRef, (snapshot) => {
+      exams = snapshot.val();
+    });
+  });
+</script>
+
+<style>
+  /* Add your styles here */
+</style>
+
+<main>
+  {#if exams && exams.length > 0}
+    {#each exams as exam (exam.key)}
+      <div class="exam">
+        <div class="logo">{exam.details.name[0]}</div>
+        <div class="details">
+          <div class="title">{exam.details.name}</div>
+          <div class="others">
+            প্রশ্নঃ {exam.questions.length} টি | সময়ঃ {exam.details.duration} মিনিট | স্কোরঃ {exam.questions.length} | নেগেটিভঃ {exam.details.negative}
+          </div>
+        </div>
+      </div>
+    {/each}
+  {:else}
+    <p>No exams available</p>
+  {/if}
+</main>
