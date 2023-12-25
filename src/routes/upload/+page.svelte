9343bf942a575319cd6fedfdc9609991a2a7eeed
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-25 12:44:22 +0600

    Update +page.svelte

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -6,16 +6,28 @@
 
   const storage = getStorage();
 
+  const handleFileChange = (event) => {
+    // Update selectedFile when the input value changes
+    selectedFile = event.target.files[0];
+    console.log('Selected file:', selectedFile);
+  };
+
   const handleFileUpload = async () => {
-    const storageRef = ref(storage, 'pwtblog/' + selectedFile.name);
-    await uploadBytes(storageRef, selectedFile);
-    console.log('File uploaded to Firebase Storage!');
-    
-    // Show an alert after successful submission
-    window.alert('File uploaded successfully!');
+    if (selectedFile) {
+      const storageRef = ref(storage, 'pwtblog/' + selectedFile.name);
+      await uploadBytes(storageRef, selectedFile);
+      console.log('File uploaded to Firebase Storage!');
+      
+      // Show an alert after successful submission
+      window.alert('File uploaded successfully!');
+    } else {
+      // Handle the case when no file is selected
+      console.error('No file selected');
+    }
   };
 
   
 </script>
 
-<input type="file" class="input" accept=".jpg, .jpeg, .png, .pdf, .mp4" on:change={handleFileUpload} />
+<input type="file" accept=".jpg, .jpeg, .png, .pdf, .mp4" on:change={handleFileChange} />
+<button on:click={handleFileUpload}>Upload</button>
