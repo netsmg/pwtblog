3d2d4355b32a184e9325cc4bcd1a6aa6b8f6e369
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-25 12:37:28 +0600

    Create +page.svelte

---
+++ src/routes/upload/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { getStorage, ref, uploadBytes } from 'firebase/storage';
+  import { onMount } from 'svelte';
+
+  let selectedFile;
+
+  const storage = getStorage();
+
+  const handleFileUpload = async () => {
+    const storageRef = ref(storage, 'pwtblog/' + selectedFile.name);
+    await uploadBytes(storageRef, selectedFile);
+    console.log('File uploaded to Firebase Storage!');
+    
+    // Show an alert after successful submission
+    window.alert('File uploaded successfully!');
+  };
+
+  
+</script>
+
+<input type="file" class="input" accept=".jpg, .jpeg, .png, .pdf, .mp4" on:change={handleFileUpload} />
