4d2bca2082d37b1cc9e051373c3e77c9d09199ee
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-26 19:40:12 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getDatabase} from "firebase/database"
 import { getFirestore } from "firebase/firestore"
+import { getStorage } from 'firebase/storage';
 import {getAuth} from 'firebase/auth'
  
 const firebaseConfig = {
@@ -20,5 +21,6 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const db = getDatabase(app);
+export const storage = getStorage(app);
 export const fstore = getFirestore(app);
 export const fauth = getAuth(app);
