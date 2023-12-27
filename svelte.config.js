9e3e6debab4577bb947919f1c5ee03677a04233d
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-27 23:19:06 +0600

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/kit/vite";
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
@@ -9,11 +8,6 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    
-    // Add manifest data
-    files: {
-      manifest: 'manifest.json',
-    },
   },
 
   preprocess: [
