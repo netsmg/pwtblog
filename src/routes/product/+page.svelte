4628dd690d3fa0875a27d6e30e9cad949f1c210a
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-27 22:11:10 +0600

    Update +page.svelte

--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -78,7 +78,30 @@
 
   // gallery Image Lightbox:
   function imageLightbox() {
-    // Implementation remains the same...
+  const lightbox = document.createElement('div');
+	lightbox.id = 'lightbox';
+	document.body.appendChild(lightbox);
+
+	const galleryImages = document.querySelectorAll('.photoContainer img');
+	galleryImages.forEach((image) => {
+		image.addEventListener('click', (e) => {
+			lightbox.classList.add('active');
+
+			const img = document.createElement('img');
+			img.src = image.src;
+			while (lightbox.firstChild) {
+				lightbox.removeChild(lightbox.firstChild);
+			}
+			lightbox.appendChild(img);
+		});
+	});
+
+	lightbox.addEventListener('click', (e) => {
+		if (e.target !== e.currentTarget) {
+			return lightbox.classList.remove('active');
+		}
+	});
+    
   }
 
   // Get Curated photos:
