302b622fb287fcfd09dfc066643cd1f306ca6b70
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-28 14:09:34 +0600

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,6 +3,9 @@ const autoprefixer = require("autoprefixer");
 
 const config = {
   plugins: [
+    skeleton({
+		themes: { preset: [ "crimson" ] }
+	})
     //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
     tailwindcss(),
     //But others, like autoprefixer, need to run after,
