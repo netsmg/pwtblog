88cab0d3efb77fd762e0ec8dba864b0fcff3b981
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-01-05 10:07:38 +0600

    Update +page.svelte

--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -1,13 +1,11 @@
 <script>
-  import { onMount } from "svelte";
+    import { onMount } from "svelte";
   import { collection, getDocs, query, orderBy } from "firebase/firestore";
   import { getFirestore } from 'firebase/firestore';
-  
-  let products = [];
+let posts = [];
   const db = getFirestore();
-  
+
   import Icon from '@iconify/svelte';
-  
   export let avatar;
   export let author;
   export let title;
@@ -17,35 +15,43 @@
   export let mins;
   export let read;
   export let commentCount = 0;
+  
+  const q = query(collection(db, 'posts'), orderBy('createdAt')); const querySnapshot = await getDocs(q); querySnapshot.forEach((doc) => { posts.push(doc.data()); // Assuming each document contains a post });
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+{#each posts as post (post.id)}
 <div on:click class="font-hind p-2 text-black dark:text-gray-400 variant-glass rounded">
-  <div class="text-xl font-bold">{title}</div>
-  <div class="text-md exp">{@html express}...</div>
+  <div class="text-xl font-bold">{post.title}</div>
+  <div class="text-md exp">{@html post.express}...</div>
   <div class="flex space-x-1 items-center">
-    <img class="w-5 h-5 max-sm:w-3 max-sm:h-3 rounded-full" src={avatar} alt="Rounded avatar" />
-    <div class="text-sm font-bold max-sm:text-[10px]">{author}</div> <Icon icon="bi:dot" /> <div class="font-lato flex read gap-1 max-sm:text-[11px]">
-      <Icon icon="ic:baseline-remove-red-eye" />{read} <Icon icon="bi:dot" /> <Icon icon="cil:comment-bubble" /> {commentCount}
-    </div>
+    <img
+      class="w-5 h-5 max-sm:w-3 max-sm:h-3 rounded-full"
+      src={avatar}
+      alt="Rounded avatar"
+    />
+    
+    <div class="text-sm font-bold max-sm:text-[10px]">{post.author}</div> <Icon icon="bi:dot" /> <div class="font-lato flex read gap-1 max-sm:text-[11px]"><Icon icon="ic:baseline-remove-red-eye" />{post.read} <Icon icon="bi:dot" /> <Icon icon="cil:comment-bubble" /> {post.commentCount} </div>
   </div>
-  <div class="flex flex-wrap gap-2 mt-2 items-center text-gray-500 font-poppins text-sm max-sm:text-[10px]">
-    <div>{createdAt}</div>
-    · {mins} min read ·
+  <div
+    class="flex flex-wrap gap-2 mt-2 items-center text-gray-500 font-poppins text-sm max-sm:text-[10px]"
+  >
+    <div>{post.createdAt}</div>
+    · {post.mins} min read ·
     {#each tags as tag, index}
-      {#if index < 3}
-        <code>{tag}</code>
+      {#if index<3}
+      <code>{post.tag}</code>
       {/if}
     {/each}
   </div>
 </div>
 <hr />
-
+{/each}
 <style>
-  .exp {
+  .exp{
     font-family: 'Lato', Kalpurush;
   }
-
-  .read {
+  .read{
     align-items: center;
   }
 </style>
