8bc1f708ec6e9fb7b7327666e8b046f342f09112
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-01-06 12:02:34 +0600

    Update +page.svelte

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -46,8 +46,10 @@
             <div class="card-content">
               <p>{project.description}</p>
             </div>
-            <button class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"><a href="{project.demo}">Demo</a></button>
-            <button class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"><a href="{project.code}">Code</a></button>
+	<div class="card-buttons">
+            <button><a href="{project.demo}">Demo</a></button>
+            <button><a href="{project.code}">Code</a></button>
+</div>
           </div>
         </div>
       {/each}
