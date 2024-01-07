b048439bddbd09fe3d581520599e1f0faeb5df95
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-01-07 12:17:21 +0600

    Update +page.svelte

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,55 +1,25 @@
 <script>
-  import { onMount } from 'svelte';
-  import { collection, onSnapshot } from 'firebase/firestore';
-  import { getFirestore } from 'firebase/firestore';
-
-  let newMessage = '';
-  let messages = [];
-  const db = getFirestore();
-
-  const query = collection(db, 'messages');
-
-  onSnapshot(query, (snapshot) => {
-    messages = snapshot.docs.map(doc => doc.data());
-  });
-
-  // Fetch messages from Firestore
-  const fetchMessages = async () => {
-    const snapshot = await db.collection('messages').get();
-    snapshot.forEach((doc) => {
-      messages.push(doc.data());
-    });
-  };
-
-  onMount(() => {
-    fetchMessages();
-
-    // Set up Firestore listener for real-time updates
-    const unsubscribe = db.collection('messages').onSnapshot((snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        if (change.type === 'added') {
-          messages.push(change.doc.data());
-        }
-      });
-    });
-
-    return () => unsubscribe(); // Cleanup listener on component destroy
-  });
-
-  // Function to send a new message
-  const sendMessage = (text) => {
-    db.collection('messages').add({
-      text,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    });
-  };
+	import Card from "../lib/components/Card.svelte";
+	import Table from "../lib/components/Table.svelte";
+	
 </script>
 
 <main>
-  {#each messages as message (message.text)}
-    <div>{message.text}</div>
-  {/each}
-
-  <textarea bind:value={newMessage} />
-  <button on:click={() => sendMessage(newMessage)}>Send</button>
+	
+	<Card>
+		<Table />
+	</Card>
 </main>
+
+<style>
+	main {
+		display: grid;
+		grid-template-columns: 100px;
+		grid-template-rows: 100px 100px;
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0px auto;
+		margin-top: 100px;
+	}
+</style>
