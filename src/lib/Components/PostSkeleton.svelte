3fb0d24b44be51490038d338e93f81760d5eac3e
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-12 13:21:19 +0600

    Update PostSkeleton.svelte

--- src/lib/Components/PostSkeleton.svelte
+++ src/lib/Components/PostSkeleton.svelte
@@ -1,19 +1,9 @@
-<div role="status" class="space-y-2.5">
-    <div class="animate-pulse w-[150px] h-3 bg-gray-400 dark:bg-gray-600 rounded-full"></div>
-    <div class="flex flex-col space-y-2 items-center">
-        <div class="w-full h-2.5 bg-gray-400 dark:bg-gray-600 rounded-full"></div>
-        <div class="w-full h-2.5 bg-gray-400 dark:bg-gray-600 rounded-full"></div>
-    </div>
-    <div class=" animate-pulse flex items-center space-x-2">
-        <div class="w-5 h-5 bg-gray-400 dark:bg-gray-600 rounded-full"></div>
-        <div class="w-[100px] h-2.5 bg-gray-400 dark:bg-gray-700 rounded-full"></div>
-    </div>
-    <div class="animate-pulse flex items-center space-x-2">
-        <div class="w-[70px] h-2.5 bg-gray-400 dark:bg-gray-700 rounded-full"></div>
-        <div class="w-[70px] h-2.5 bg-gray-400 dark:bg-gray-600 rounded-full"></div>
-        <div class="w-[30px] h-2.5 bg-gray-400 dark:bg-gray-700 rounded-full"></div>
-        <div class="w-[30px] h-2.5 bg-gray-400 dark:bg-gray-600 rounded-full"></div>
-        <div class="w-[30px] h-2.5 bg-gray-400 dark:bg-gray-700 rounded-full"></div>
-    </div>
-</div>
-<hr/>
+<script>
+  import { TextPlaceholder } from 'flowbite-svelte';
+</script>
+
+<TextPlaceholder />
+<TextPlaceholder size="md" class="mt-8" />
+<TextPlaceholder size="lg" class="mt-8" />
+<TextPlaceholder size="xl" class="mt-8" />
+<TextPlaceholder size="xxl" class="mt-8" />
